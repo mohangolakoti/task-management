9c15b9ab7849579fd00cb0897efa7cd35ce8962b
mohangolakoti <ramgolakoti1@gmail.com>
2025-09-05 05:05:12 +0530

    api url

--- frontend/src/data/api.js
+++ frontend/src/data/api.js
@@ -1,2 +1,2 @@
 //export const API_URL = "http://localhost:4000";
-export const API_URL = "https://task-management-a8oj.onrender.com/";
+export const API_URL = "https://task-management-a8oj.onrender.com";
